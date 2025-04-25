4b3f2ef1e8c1c5920f821d2847ffb532e52c9ae7
Fleacon <146029061+Fleacon@users.noreply.github.com>
2025-04-25 11:02:21 +0200

    create Player, Rechner, Waffe shell

--- src/Rechner.java
+++ src/Rechner.java
@@ -1,2 +1,17 @@
 public class Rechner {
+    private static Rechner instance;
+    private Player player;
+    private Mob mob;
+
+    private Rechner() {
+
+    }
+
+    public static Rechner getInstance() {
+        if (instance == null)
+            instance = new Rechner();
+        return instance;
+    }
+
+
 }

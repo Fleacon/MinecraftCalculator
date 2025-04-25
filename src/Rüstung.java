67844c68e577ee8ac9a3f34e2de19056f60faf21
Lvgga <wenzelluka@gmail.com>
2025-04-25 10:47:19 +0200

    Update Rüstung.java
    Hülle Rüstung :
    - Atribute
    - Konstruktor
    - get + set - Methoden
    - toString
    - Hülle logik Methoden

--- src/Rüstung.java
+++ src/Rüstung.java
@@ -1,2 +1,48 @@
+import Verzauberungen.Verbrennung;
+import Verzauberungen.Verzauberung;
+
 public class Rüstung {
+    private String körperteil;
+    private String material;
+    private double rüstungsPunkte;
+    public Rüstung(String körperteil, String material, double rüstungsPunkte){
+        this.körperteil = körperteil;
+        this.material = material;
+        this.rüstungsPunkte = rüstungsPunkte;
+    }
+    public void verzaubern(Verzauberung verzauberung){
+        
+    }
+    public static int berechneSchadensreduzierung(){
+
+    }
+
+    public String getKörperteil() {
+        return körperteil;
+    }
+
+    public void setKörperteil(String körperteil) {
+        this.körperteil = körperteil;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public double getRüstungsPunkte() {
+        return rüstungsPunkte;
+    }
+
+    public void setRüstungsPunkte(double rüstungsPunkte) {
+        this.rüstungsPunkte = rüstungsPunkte;
+    }
+
+    public String toString() {
+        String a = "Körperteil : " + körperteil + "\nMaterial : " + material + "\nRüstungspunkte : " + rüstungsPunkte;
+        return a;
+    }
 }
